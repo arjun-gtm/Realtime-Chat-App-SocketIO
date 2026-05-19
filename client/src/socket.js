45ffe4192ca490522238ef8204332003b47e469b
arjun-gtm <gtmarjun10@gmail.com>
2026-05-19 16:14:47 +0545

    Realtime Chat App- MERN Stack

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
+const SOCKET_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:4000"
+    : "https://realtime-chat-app-socketio-2lsu.onrender.com";
+
+const socket = io(SOCKET_URL, {
   autoConnect: false,
 });
 
